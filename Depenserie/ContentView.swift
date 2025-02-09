0df55a3879c28b45b1105aea20644ef1edd121c2
Anthony Li <me@anli.dev>
2025-02-09 01:06:15 -0500

    Initial commit

---
+++ Depenserie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Depenserie
+//
+//  Created by Anthony Li on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Depenserie/DepenserieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepenserieApp.swift
+//  Depenserie
+//
+//  Created by Anthony Li on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DepenserieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
